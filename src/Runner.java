1b40e4d829de306a43329664d22dbbb7b8d1f374
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-10-26 10:15:45 -0400

    Added edits to runner.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,21 @@
 public class Runner
 {
-    String[][] face = new String[5][8];
+    public static void main(String args[])
+    {
+        String[][] face = new String[5][8];
 
-    JackOLantern pump = new JackOLantern(face);
-    
+        JackOLantern pump = new JackOLantern(face);
+        pump.fill("X");
+
+        pump.edit("",0,0);
+        pump.edit("",1,0);
+        pump.edit("",3,0);
+        pump.edit("",4,0);
+        pump.edit("",0,7);
+        pump.edit("",1,7);
+        pump.edit("",3,7);
+        pump.edit("",4,7);
+
+        System.out.println(pump);
+    }
 }
