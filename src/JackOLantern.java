d9f8c482392429bf1aab1a1e7b169450121859c4
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-10-27 19:57:58 -0700

    Finished runner and comments.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,17 +1,33 @@
+/**Mir Shahiduzzaman*/
+
 public class JackOLantern extends java.lang.Object
 {
     private String[][] faceFeatures;
 
+    /**
+     * Constructor method to fill in private array faceFeatures with parameter faceFeatures
+     * @param faceFeatures 2D array replacing the private 2D array faceFeatures
+     */
     public JackOLantern(java.lang.String[][] faceFeatures)
     {
         this.faceFeatures = faceFeatures;
     }
 
+    /**
+     * replaces a chosen space with a character
+     * @param replace the character that will replace the chosen space
+     * @param row row to replace
+     * @param column column to replace
+     */
     public void edit(java.lang.String replace, int row, int column)
     {
         faceFeatures[row][column] = replace;
     }
 
+    /**
+     * fills in the private array faceFeatures with a string
+     * @param str string that will fill in all spaces in faceFeatures
+     */
     public void fill(java.lang.String str)
     {
         for(int i = 0;i<this.faceFeatures.length;i++)
@@ -23,6 +39,10 @@ public class JackOLantern extends java.lang.Object
         }
     }
 
+    /**
+     * overrides the default toString() method to return a String concatenation of the faceFeatures array
+     * @return a string with the concatenation of the private faceFeatures array
+     */
     @Override
     public String toString()
     {
--- src/Runner.java
+++ src/Runner.java
@@ -1,47 +1,62 @@
+/**Mir Shahiduzzaman*/
+
 public class Runner
 {
     public static void main(String args[])
     {
-        String[][] face = new String[6][10];
+        String[][] face = new String[6][12];
 
+        //makes JackOLantern object filled with X's
         JackOLantern pump = new JackOLantern(face);
         pump.fill("X");
 
+        //makes left bolt
         pump.edit(" ",0,0);
         pump.edit(" ",1,0);
         pump.edit(" ",3,0);
         pump.edit(" ",4,0);
         pump.edit(" ",5,0);
 
-        pump.edit(" ",0,9);
-        pump.edit(" ",1,9);
-        pump.edit(" ",3,9);
-        pump.edit(" ",4,9);
-        pump.edit(" ",5,9);
+        //makes right bolt
+        pump.edit(" ",0,11);
+        pump.edit(" ",1,11);
+        pump.edit(" ",3,11);
+        pump.edit(" ",4,11);
+        pump.edit(" ",5,11);
 
+        //creates forehead
         pump.edit(" ",1,2);
         pump.edit(" ",1,3);
         pump.edit(" ",1,4);
         pump.edit(" ",1,5);
         pump.edit(" ",1,6);
         pump.edit(" ",1,7);
+        pump.edit(" ",1,8);
+        pump.edit(" ",1,9);
 
-
+        //makes spaces for eyes
         pump.edit(" ",3,2);
-        pump.edit(" ",3,4);
+        pump.edit(" ",3,3);
         pump.edit(" ",3,5);
-        pump.edit(" ",3,7);
+        pump.edit(" ",3,6);
+        pump.edit(" ",3,8);
+        pump.edit(" ",3,9);
 
+        //makes space for mouth
         pump.edit(" ",4,2);
-        pump.edit(" ",4,7);
+        pump.edit(" ",4,9);
 
+        //mouth of pumpkin
         pump.edit("-",4,3);
         pump.edit("-",4,4);
         pump.edit("-",4,5);
         pump.edit("-",4,6);
+        pump.edit("-",4,7);
+        pump.edit("-",4,8);
 
-        pump.edit("O",3,3);
-        pump.edit("O",3,6);
+        //eyes of pumpkin
+        pump.edit("O",3,4);
+        pump.edit("O",3,7);
 
         System.out.println(pump);
     }
